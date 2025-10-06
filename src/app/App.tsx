39e34fe4f137677a63fa6a563e346530f4372308
João Victor Vardenski de Andrade <126905465+joaovardenski@users.noreply.github.com>
2025-10-06 15:14:42 -0300

    Removendo partes inutilizadas do router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,9 +4,6 @@ import Login from "../pages/Auth/Login";
 import DashboardFuncionario from "../pages/Funcionario/DashboardFuncionario";
 import DashboardAdmin from "../pages/Admin/DashboardAdmin";
 import RelatoriosAdmin from "../pages/Admin/RelatoriosAdmin";
-// import PrivateRoute from "../routes/PrivateRoute";
-// import AdminRoute from "../routes/AdminRoute";
-
 
 const router = createBrowserRouter([
   {
@@ -18,11 +15,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: (
-          //<PrivateRoute>
-          <DashboardFuncionario />
-          //</PrivateRoute>
-        ),
+        element: <DashboardFuncionario />,
       },
     ],
   },
@@ -31,23 +24,14 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: (
-          //<AdminRoute>
-            <DashboardAdmin />
-          //</AdminRoute>
-        
-      ),
-    },
-    {
-        path: "/admin/relatorios",
-        element: (
-          //<AdminRoute>
-            <RelatoriosAdmin />
-          //</AdminRoute>
-        ),
+        element: <DashboardAdmin />,
       },
-    ]
-  }
+      {
+        path: "/admin/relatorios",
+        element: <RelatoriosAdmin />,
+      },
+    ],
+  },
 ]);
 
 function App() {
